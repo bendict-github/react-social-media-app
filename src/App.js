18b136b2fc3c1fa8c5cbca91554c9ffce6a44a5b
Chirag Bellani <112893293+bendict-github@users.noreply.github.com>
2023-12-29 23:07:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import Home from "./pages/home/Home";
+import React from "react";
+// import Profile from "./pages/profile/Profile";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+
+
+
+function App() {
+    return (
+        <Home />
+    )
+}
+
+
+export default App;
